404f41c4bbf0bf40ac8476423cfc88eefc57cf85
lisperow <lisperow@yandex.ru>
2019-10-26 11:34:47 +0300

    init

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject csvdb "0.1.0-SNAPSHOT"
+  :description "tasks of clojure course"
+  :url "http://github.com/lisperow/clojure-example"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clojure-csv "2.0.1"]])
